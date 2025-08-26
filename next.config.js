e1bc4484bac89382d8efc888a1d3d1e855e02c1d
leadwithvicky <vicky@visiontech.world>
2025-08-26 19:08:54 +0530

    updated pdf

--- next.config.js
+++ next.config.js
@@ -6,8 +6,19 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imghippo.com',
       }
     ]
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('pdfkit', 'fontkit', 'restructure', 'iconv-lite');
+    }
+    return config;
   }
 };
 
